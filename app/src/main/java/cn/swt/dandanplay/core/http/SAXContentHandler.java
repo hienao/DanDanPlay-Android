3ada9d5b1381894fcfd7dce0545c6cdb77c7e750
shiwentao666 <shiwentao666@gmail.com>
2016-12-26 17:48:48 +0800

    颜色转换
    Signed-off-by: shiwentao666 <shiwentao666@gmail.com>

--- app/src/main/java/cn/swt/dandanplay/core/http/SAXContentHandler.java
+++ app/src/main/java/cn/swt/dandanplay/core/http/SAXContentHandler.java
@@ -2,9 +2,16 @@ package cn.swt.dandanplay.core.http;
 
 import android.util.Log;
 
+import com.swt.corelib.utils.ColorUtils;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.swt.dandanplay.core.http.beans.CommentResponse;
 
 /**
  * Title: SAXContentHandler <br>
@@ -16,20 +23,32 @@ import org.xml.sax.helpers.DefaultHandler;
 public class SAXContentHandler extends DefaultHandler {
     //声明标签的名称
     public String tagName;
-    private StringBuffer buffer = new StringBuffer();
+    private List<CommentResponse.CommentsBean> mBiliCommentsBeanList ;
+    private CommentResponse.CommentsBean mCommentsBean;
     @Override
     public void startDocument() throws SAXException {
         super.startDocument();
+        mBiliCommentsBeanList =new ArrayList<>();
     }
 
     @Override
     public void endDocument() throws SAXException {
         super.endDocument();
+        mBiliCommentsBeanList=null;
     }
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         if (localName.equals("d")) {
+            mCommentsBean=new CommentResponse.CommentsBean();
+            String attaibute=attributes.getValue(0);
+            String[] sourceStrArray = attaibute.split(",");
+            mCommentsBean.setTime(Double.parseDouble(sourceStrArray[0]));//设置弹幕出现的时间 以秒数为单位。
+            mCommentsBean.setMode(Integer.parseInt(sourceStrArray[1]));//弹幕的模式
+            int color= ColorUtils.bilicolor2dandancolor(sourceStrArray[2]);
+            if (color!=-1){
+                mCommentsBean.setColor(color);
+            }
             Log.e("sax",attributes.getValue(0));
             tagName=localName;
         }
@@ -40,6 +59,7 @@ public class SAXContentHandler extends DefaultHandler {
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
         tagName=null;
+        mCommentsBean=null;
     }
 
     @Override
@@ -50,7 +70,9 @@ public class SAXContentHandler extends DefaultHandler {
             String data=new String(ch,start,length);
             //判断标签是否为空
             if(tagName.equals("d")){
-                Log.e("sax",data);
+                if (mCommentsBean!=null){
+                    mCommentsBean.setMessage(data);
+                }
             }
         }
     }
---
+++ corelib/src/main/java/com/swt/corelib/utils/ColorUtils.java
@@ -0,0 +1,29 @@
+package com.swt.corelib.utils;
+
+import android.text.TextUtils;
+
+/**
+ * Title: ColorUtils <br>
+ * Description: 颜色转换工具<br>
+ * Copyright (c) 传化物流版权所有 2016 <br>
+ * Created DateTime: 2016/12/26 17:34
+ * Created by Wentao.Shi.
+ */
+public class ColorUtils {
+    /**
+     * 将bilibili弹幕xml信息中的颜色字符串转化为dandanplay的颜色信息
+     * @param bilicolor        10位16进制颜色字符串
+     * @return 32位整形数的弹幕颜色，算法为 R*256*256 + G*256 + B。
+     */
+    public static int bilicolor2dandancolor(String bilicolor){
+        if (bilicolor==null)
+            return -1;
+        if (TextUtils.isDigitsOnly(bilicolor)&&bilicolor.length()==8){
+            int r=Integer.parseInt(bilicolor.substring(2,4),16);
+            int g=Integer.parseInt(bilicolor.substring(4,6),16);
+            int b=Integer.parseInt(bilicolor.substring(6,8),16);
+            return r*256*256+g*256+b;
+        }
+        return -1;
+    }
+}
