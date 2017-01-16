97d3e61f654cf3610abddaee05ce330c34822f84
shiwentao666 <shiwentao666@gmail.com>
2017-01-16 19:14:37 +0800

    去除测试代码

--- app/src/main/java/cn/swt/dandanplay/fileexplorer/view/ContentsActivity.java
+++ app/src/main/java/cn/swt/dandanplay/fileexplorer/view/ContentsActivity.java
@@ -15,7 +15,6 @@ import android.support.v7.widget.RecyclerView;
 
 import com.swt.corelib.utils.ToastUtils;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,11 +31,6 @@ import cn.swt.dandanplay.fileexplorer.component.DaggerMainComponent;
 import cn.swt.dandanplay.fileexplorer.contract.MainContract;
 import cn.swt.dandanplay.fileexplorer.module.MainModule;
 import cn.swt.dandanplay.fileexplorer.presenter.MainPresenter;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class ContentsActivity extends BaseActivity implements MainContract.View {
 
@@ -64,7 +58,6 @@ public class ContentsActivity extends BaseActivity implements MainContract.View 
         initData();
         initView();
         initListener();
-        test2();
     }
 
     private void initData() {
@@ -154,43 +147,6 @@ public class ContentsActivity extends BaseActivity implements MainContract.View 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    }
-
-
-    private void test() {
-
-
-        OkHttpClient mOkHttpClient = new OkHttpClient();
-        Request.Builder requestBuilder = new Request.Builder()
-//                .removeHeader("User-Agent").addHeader("User-Agent","Mozilla/5.0 BiliDroid/4.33.3 (bbcallen@gmail.com)")
-//                .url("http://newbarrage.bilibilijj.com/api/down/9846894/2017-01-15/1/bilibilijj%40Point%40com-2016@Blank@vma%e6%9c%80%e4%bd%b3%e8%a7%86%e8%a7%89%e6%95%88%e6%9e%9c%e5%a4%a7%e5%a5%96%e2%80%94%e2%80%94coldplay@Blank@-@Blank@up%40And%40up/9BE5AFAED559CAADD0207B66FE71A81F/1484448551")
-                .url("https://comment.bilibili.com/6065590.xml");
-        final Request request = requestBuilder.build();
-        Call mcall = mOkHttpClient.newCall(request);
-        mcall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-//
-//                    byte[] bytes = response.body().bytes(); //获取数据的bytes
-//                    InputStream inputStream=new ByteArrayInputStream(bytes);
-//                    FileUtils.createFileByDeleteOldFile("sdcard/test.xml");
-//                    FileUtils.writeFileFromIS("sdcard/test.xml",inputStream,true);
-//                    String content = FileUtils.readFile2String("sdcard/test.xml","UTF-8");
-                    String content = response.body().string();
-                    System.out.println(content);
-                }
-            }
-        });
-    }
-
-    public void test2() {
-
     }
 
 }
