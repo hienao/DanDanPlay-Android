a523808d82b6898adec679a9e5685af20955fab6
shiwentao <6790572>
2017-02-12 16:40:18 +0800

    更换播放器SDK，修复bug

--- app/src/main/java/cn/swt/danmuplayer/play/view/VideoViewActivity.java
+++ app/src/main/java/cn/swt/danmuplayer/play/view/VideoViewActivity.java
@@ -1,37 +1,36 @@
 package cn.swt.danmuplayer.play.view;
 
 import android.content.Context;
-import android.content.res.Configuration;
-import android.os.Build;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.KeyEvent;
 
-import com.superplayer.library.SuperPlayer;
+import com.dl7.player.media.IjkPlayerView;
+import com.swt.corelib.utils.FileUtils;
 import com.swt.corelib.utils.LogUtils;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import cn.swt.danmuplayer.R;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import cn.swt.danmuplayer.play.contract.VideoViewContract;
 import cn.swt.danmuplayer.play.presenter.VideoViewPresenter;
 
-public class VideoViewActivity extends AppCompatActivity implements VideoViewContract.View, View.OnClickListener, SuperPlayer.OnNetChangeListener {
+public class VideoViewActivity extends AppCompatActivity implements VideoViewContract.View {
     VideoViewPresenter mVideoViewPresenter;
-    @BindView(R.id.activity_video_view)
-    RelativeLayout mActivityVideoView;
-    @BindView(R.id.view_super_player)
-    SuperPlayer mViewSuperPlayer;
+    com.dl7.player.media.IjkPlayerView mViewSuperPlayer;
     private String videoPath, videoTitle, file_title;
-    private boolean hide_danmu=false,danmuPrepare=false,playerPrepare=false;
+    private boolean hide_danmu = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_view);
-        ButterKnife.bind(this);
+        mViewSuperPlayer = new IjkPlayerView(this);
+        setContentView(mViewSuperPlayer);
         initData();
         initPlayer();
         initView();
@@ -46,12 +45,11 @@ public class VideoViewActivity extends AppCompatActivity implements VideoViewCon
 
 
     private void initData() {
-        mVideoViewPresenter=new VideoViewPresenter(this);
+        mVideoViewPresenter = new VideoViewPresenter(this);
         videoPath = getIntent().getStringExtra("path");
         file_title = getIntent().getStringExtra("file_title");
         videoTitle = getIntent().getStringExtra("title");
-        hide_danmu=getIntent().getBooleanExtra("hide_danmu",false);
-        mVideoViewPresenter = new VideoViewPresenter(this);
+        hide_danmu = getIntent().getBooleanExtra("hide_danmu", false);
     }
 
     private void initView() {
@@ -60,92 +58,116 @@ public class VideoViewActivity extends AppCompatActivity implements VideoViewCon
         } else {
             mViewSuperPlayer.setTitle(file_title);
         }
-        mViewSuperPlayer.play(videoPath);
-        LogUtils.i("播放器设置路径");
-        mViewSuperPlayer.pause();
-        LogUtils.i("播放器暂停");
     }
 
     private void initPlayer() {
-        mViewSuperPlayer.setLive(false);//设置该地址是直播的地址
-        mViewSuperPlayer.setNetChangeListener(true)//设置监听手机网络的变化
-                .setOnNetChangeListener(this)//实现网络变化的回调
-                .onPrepared(new SuperPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared() {
-                        /**
-                         * 监听视频是否已经准备完成开始播放。（可以在这里处理视频封面的显示跟隐藏）
-                         */
-                        playerPrepare=true;
-                        if (danmuPrepare){
-                            mViewSuperPlayer.start();
-                            LogUtils.i("播放器开始");
-                            mViewSuperPlayer.getDanmakuView().resume();
-                            LogUtils.i("弹幕显示");
-                        }
 
-                    }
-                }).onComplete(new Runnable() {
-            @Override
-            public void run() {
-                /**
-                 * 监听视频是否已经播放完成了。（可以在这里处理视频播放完成进行的操作）
-                 */
+        String danmuxml = "";
+        InputStream inputStream=null;
+        if (!TextUtils.isEmpty(videoPath)) {
+            String ddxml = videoPath.substring(0, videoPath.lastIndexOf(".")) + "dd.xml";
+            String bilixml = videoPath.substring(0, videoPath.lastIndexOf(".")) + ".xml";
+            if (FileUtils.isFileExists(ddxml)) {
+                danmuxml = ddxml;
+            } else if (FileUtils.isFileExists(bilixml)) {
+                danmuxml = bilixml;
             }
-        }).onInfo(new SuperPlayer.OnInfoListener() {
-            @Override
-            public void onInfo(int what, int extra) {
-                /**
-                 * 监听视频的相关信息。
-                 */
-
-            }
-        }).onError(new SuperPlayer.OnErrorListener() {
-            @Override
-            public void onError(int what, int extra) {
-                /**
-                 * 监听视频播放失败的回调
-                 */
-
-            }
-        }).onDanmuViewPrepared(new SuperPlayer.OnDanmuViewPreparedListener() {
-            @Override
-            public void onPrepared() {
-                danmuPrepare=true;
-                if (playerPrepare){
-                    mViewSuperPlayer.start();
-                    LogUtils.i("播放器开始");
-                    mViewSuperPlayer.getDanmakuView().resume();
-                    LogUtils.i("弹幕显示");
-                }
-
-            }
-        });;
-        mViewSuperPlayer.initDanmuView(videoPath,hide_danmu);
-        mViewSuperPlayer.setScaleType(SuperPlayer.SCALETYPE_FITXY);
-        mViewSuperPlayer.setPlayerWH(0, mViewSuperPlayer.getMeasuredHeight());//设置竖屏的时候屏幕的高度，如果不设置会切换后按照16:9的高度重置
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+
+        if (!TextUtils.isEmpty(danmuxml) && FileUtils.isFileExists(danmuxml)) {
+            try {
+                inputStream = new FileInputStream(new File(danmuxml));
+
+            } catch (FileNotFoundException e) {
+                LogUtils.e("cuowu", e);
+            }
+        }
+        //缩略图
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(videoPath);
+        Bitmap bitmap = retriever.getFrameAtTime();
+        mViewSuperPlayer.mPlayerThumb.setImageBitmap(bitmap);
+        //        Glide.with(this).load(IMAGE_URL).fitCenter().into(mPlayerView.mPlayerThumb); // Show the thumb before play
+        mViewSuperPlayer.init()              // Initialize, the first to use
+                .alwaysFullScreen()//始终全屏
+//                .setSkipTip(1000 * 60 * 0)  // set the position you want to skip
+                .enableOrientation()    // enable orientation
+                .setVideoPath(videoPath)    // set video url
+//                .setVideoSource(null, VIDEO_URL, VIDEO_URL, VIDEO_URL, null) // set multiple video url
+//                .setMediaQuality(IjkPlayerView.MEDIA_QUALITY_HIGH)  // set the initial video url
+                .enableDanmaku()        // enable Danmaku
+                .setDanmakuSource(inputStream)
+                .showOrHideDanmaku(!hide_danmu)
+                .setTitle(file_title)      // set title
+                .setQualityButtonVisibility(false)//需要在全屏后使用生效
+           ;
+
+
+//        mViewSuperPlayer.setLive(false);//设置该地址是直播的地址
+//        mViewSuperPlayer.setNetChangeListener(true)//设置监听手机网络的变化
+//                .setOnNetChangeListener(this)//实现网络变化的回调
+//                .onPrepared(new SuperPlayer.OnPreparedListener() {
+//                    @Override
+//                    public void onPrepared() {
+//                        /**
+//                         * 监听视频是否已经准备完成开始播放。（可以在这里处理视频封面的显示跟隐藏）
+//                         */
+//                        playerPrepare = true;
+//                        if (danmuPrepare) {
+//                            mViewSuperPlayer.start();
+//                            LogUtils.i("播放器开始");
+//                            mViewSuperPlayer.getDanmakuView().resume();
+//                            LogUtils.i("弹幕显示");
+//                        }
+//
+//                    }
+//                }).onComplete(new Runnable() {
+//            @Override
+//            public void run() {
+//                /**
+//                 * 监听视频是否已经播放完成了。（可以在这里处理视频播放完成进行的操作）
+//                 */
+//            }
+//        }).onInfo(new SuperPlayer.OnInfoListener() {
+//            @Override
+//            public void onInfo(int what, int extra) {
+//                /**
+//                 * 监听视频的相关信息。
+//                 */
+//
+//            }
+//        }).onError(new SuperPlayer.OnErrorListener() {
+//            @Override
+//            public void onError(int what, int extra) {
+//                /**
+//                 * 监听视频播放失败的回调
+//                 */
+//
+//            }
+//        }).onDanmuViewPrepared(new SuperPlayer.OnDanmuViewPreparedListener() {
+//            @Override
+//            public void onPrepared() {
+//                danmuPrepare = true;
+//                if (playerPrepare) {
+//                    mViewSuperPlayer.start();
+//                    LogUtils.i("播放器开始");
+//                    mViewSuperPlayer.getDanmakuView().resume();
+//                    LogUtils.i("弹幕显示");
+//                }
+//
+//            }
+//        });
+//        ;
+//        mViewSuperPlayer.initDanmuView(videoPath, hide_danmu);
+//        mViewSuperPlayer.setScaleType(SuperPlayer.SCALETYPE_FITXY);
+//        mViewSuperPlayer.setPlayerWH(0, mViewSuperPlayer.getMeasuredHeight());//设置竖屏的时候屏幕的高度，如果不设置会切换后按照16:9的高度重置
     }
+
 
     @Override
     public Context getContext() {
         return this;
     }
-
 
 
     @Override
@@ -156,90 +178,34 @@ public class VideoViewActivity extends AppCompatActivity implements VideoViewCon
     @Override
     protected void onResume() {
         super.onResume();
-        if (mViewSuperPlayer != null) {
-            mViewSuperPlayer.onResume();
-        }
+        mViewSuperPlayer.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mViewSuperPlayer != null) {
-            mViewSuperPlayer.onPause();
-        }
+        mViewSuperPlayer.onPause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mViewSuperPlayer != null) {
-            mViewSuperPlayer.onDestroy();
-        }
+        mViewSuperPlayer.onDestroy();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (mViewSuperPlayer != null) {
-            mViewSuperPlayer.onConfigurationChanged(newConfig);
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (mViewSuperPlayer.handleVolumeKey(keyCode)) {
+            return true;
         }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     public void onBackPressed() {
-        if (mViewSuperPlayer != null && mViewSuperPlayer.onBackPressed()) {
-            finish();
-//            return;
+        if (mViewSuperPlayer.onBackPressed()) {
+            return;
         }
         super.onBackPressed();
-    }
-
-    @Override
-    public void onClick(View view) {
-//        if(view.getId() == R.id.tv_replay){
-//            if(player != null){
-//                player.play(url);
-//            }
-//        } else if(view.getId() == R.id.tv_play_location){
-//            if(isLive){
-//                Toast.makeText(this,"直播不支持指定播放",Toast.LENGTH_SHORT).show();
-//                return;
-//            }
-//            if(player != null){
-//                /**
-//                 * 这个节点是根据视频的大小来获取的。不同的视频源节点也会不一致（一般用在退出视频播放后保存对应视频的节点从而来达到记录播放）
-//                 */
-//                player.play(url,89528);
-//            }
-//        } else if(view.getId() == R.id.tv_play_switch) {
-//            /**
-//             * 切换视频播放源（一般是标清，高清的切换ps：由于我没有找到高清，标清的视频源，所以也是换相同的地址）
-//             */
-//            if(isLive){
-//                player.playSwitch(url);
-//            } else {
-//                player.playSwitch("http://baobab.wandoujia.com/api/v1/playUrl?vid=2614&editionType=high");
-//            }
-//        }
-    }
-
-    @Override
-    public void onWifi() {
-
-    }
-
-    @Override
-    public void onMobile() {
-
-    }
-
-    @Override
-    public void onDisConnect() {
-
-    }
-
-    @Override
-    public void onNoAvailable() {
-
     }
 }
